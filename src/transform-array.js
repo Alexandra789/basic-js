a8c4d40f7c014c84a7c634d7566bdf087cdb6237
unknown <zakovinko99@gmail.com>
2020-03-04 01:21:26 +0300

    feat: done tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,22 +1,33 @@
-module.exports = 
-function transform( arr ) {
-    if (!Array.isArray(arr)) {
-        throw "Error";
-    }
-    else if(arr[0] === 'undefined' && arr.length <=1){return [];}
-    else{
-        for(let i = 0; i<arr.length; i++){
-        switch(arr[i]){
-            case '--discard-next': arr.length <=2 ? arr = [] : arr.splice(i,2); break;
-            case '--discard-prev': arr.length <=2 && arr[0] === '--discard-prev' ? arr = [] : arr.splice(i-1,2); break;
-            case '--double-next' : arr.length <=2 ? arr = [] : arr[i] = arr[i+1]; break;
-            case '--double-prev' : arr.length <=2 ? arr = [] : arr[i] = arr[i-1]; break;
-            default: arr[i]; break;
+module.exports =
+    function transform(arr) {
+        let result = []
+        if (!Array.isArray(arr)) {
+            throw "Error";
+        }
+        // else if (arr[0] === 'undefined' && arr.length <= 1) { return []; } 
+        // else {
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i]) {
+                case '--discard-next':
+                    i++;
+                    break;
+                case '--discard-prev':
+                    result.pop();
+                    break;
+                case '--double-next':
+                    if (i + 1 !== arr.length) {
+                        result.push(arr[i + 1]);
+                    }
+                    break;
+                case '--double-prev':
+                    if (i - 1 > 0) {
+                        result.push(arr[i - 1]);
+                    }
+                    break;
+                default:
+                    result.push(arr[i]);
+                    break;
             }
         }
-    }
-    return arr;
-};
-
-// console.log(transform(['--discard-next']));
-//[1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
+        return result;
+    };
